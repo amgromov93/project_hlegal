038c03dd109054567d3db80c31197a5fb934be68
Gromov Aleksey <amgromov93@gmail.com>
2022-07-18 12:45:28 +0200

    created mobile-first adaptive for page about us

--- app/js/all.js
+++ app/js/all.js
@@ -44,32 +44,18 @@ $('.js-slider').slick({
     dots: true,
     infinite: false,
     speed: 200,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 2000,
     responsive: [
       {
-        breakpoint: 991,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
           dots: true
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
         }
       }
     ]
